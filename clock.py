ac90d114fb6655aa4b0a7a66570741f368c6551a
karvus-hn <61095185+karvus-hn@users.noreply.github.com>
2020-03-09 23:52:02 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,13 +3,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from lab3 import db,user,datetime,json,requests,url
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', seconds=10)
+@sched.scheduled_job('interval', seconds=60)
 def timed_job():
     queryA = db.session.query(user)
     for u in queryA.all():
         dif= datetime.now()-u.lastans
         params={'chat_id':u.tg_id,'text':'t'}
-        if dif.seconds>60:
+        if dif.seconds>1800:
             params['text']='Повторяй'
             reply = {'keyboard': [[{'text': 'Повторить'}], [{'text': 'Отложить'}]], 'resize_keyboard': True}
             reply = json.dumps(reply)
