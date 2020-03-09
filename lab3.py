10711c085cc6928579c592cbffbc268c1f110a89
karvus-hn <61095185+karvus-hn@users.noreply.github.com>
2020-03-09 22:16:07 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -77,6 +77,7 @@ def GRound(chat_id,params):
                 db.session.add(tempL)
                 #db.session.flush()
                 rezC=tempL
+			db.session.commit()
             rt=rezC.cnt
             if (rt<=ccount):
                 break
@@ -122,7 +123,7 @@ def webhook():
                     tempU=user(tg_id=chat_id,lastans=datetime.utcnow())
                     db.session.add(tempU)
                     #db.session.flush()
-
+				db.session.commit()
             params={'chat_id':chat_id,'text':text}
             st=params['text'].split(' ')
 
@@ -140,14 +141,14 @@ def webhook():
                     data={"id":chat_id,"word":dct[chat_id].Cword,"dt":datetime.utcnow()}
                     rezL=queryB.filter(learning.user_id==chat_id,learning.word==dct[chat_id].Cword ).first()   # находим слово ++
                     rezL.cnt+=1
-                    #db.session.flush()
+                    db.session.commit()
                     params['text']='Правильно {cor} раз(а)'.format(cor=rezL.cnt)
 
                 else:
                     params['text']='Неправильно.Правильный ответ {word}.'.format(word=dct[chat_id].Cword)
                 rez =queryA.filter(user.tg_id==chat_id).first()
                 rez.lastans=datetime.utcnow()
-                #db.session.flush()
+                db.session.commit()
                 requests.post(url=url+'/sendMessage',data=params)
                 dct[chat_id].rnd+=1
                 if dct[chat_id].rnd==10:
